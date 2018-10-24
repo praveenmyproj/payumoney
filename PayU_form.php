4f260497d32b1c2d857ac56c5582a61d1181b56c
praveenmyproj <31179569+praveenmyproj@users.noreply.github.com>
2018-10-24 15:08:23 +0530

    Add files via upload

--- PayU_form.php
+++ PayU_form.php
@@ -62,6 +62,27 @@
   	.hd{
   		display: none;
   	}
+    .form-control {
+      display: block;
+      width: 100%;
+      padding: .375rem .75rem;
+      font-size: 1rem;
+      line-height: 1.5;
+      color: #495057;
+      background-color: #fff;
+      background-clip: padding-box;
+      border: 1px solid #ced4da;
+      border-radius: .25rem;
+      transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    }
+    .pay-form table {
+      width: 50%;
+      margin: 20px 20px;
+    }
+    .pay-form table > tbody, .pay-form table > tbody > tr, .pay-form table > tbody, .pay-form table > tbody > tr > td {
+      display: block;
+      margin-bottom: 10px;
+    }
   </style>	
   <script>
     var hash = '<?php echo $hash ?>';
@@ -82,34 +103,34 @@
       <br/>
       <br/>
     <?php } ?>
-    <form action="<?php echo $action; ?>" method="post" name="payuForm">
+    <form class="pay-form" action="<?php echo $action; ?>" method="post" name="payuForm">
       <input type="hidden" name="key" value="<?php echo $merchant_key ?>" />
       <input type="hidden" name="hash" value="<?php echo $hash ?>"/>
       <input type="hidden" name="txnid" value="<?php echo $txnid ?>" />
       <table>
         <tr>
           <td>Amount <span class="mand">*</span>: </td>
-          <td><input name="amount" type="number" value="<?php echo (empty($posted['amount'])) ? '' : $posted['amount'] ?>" /></td>
+          <td><input class="form-control" name="amount" type="number" value="<?php echo (empty($posted['amount'])) ? '' : $posted['amount'] ?>" /></td>
           <td>First Name <span class="mand">*</span>: </td>
-          <td><input type="text" name="firstname" id="firstname" value="<?php echo (empty($posted['firstname'])) ? '' : $posted['firstname']; ?>" /></td>
+          <td><input class="form-control" type="text" name="firstname" id="firstname" value="<?php echo (empty($posted['firstname'])) ? '' : $posted['firstname']; ?>" /></td>
         </tr>
         <tr>
           <td>Email <span class="mand">*</span>: </td>
-          <td><input type="email" name="email" id="email" value="<?php echo (empty($posted['email'])) ? '' : $posted['email']; ?>" /></td>
+          <td><input class="form-control" type="email" name="email" id="email" value="<?php echo (empty($posted['email'])) ? '' : $posted['email']; ?>" /></td>
           <td>Phone <span class="mand">*</span>: </td>
-          <td><input type="text" name="phone" value="<?php echo (empty($posted['phone'])) ? '' : $posted['phone']; ?>" /></td>
+          <td><input class="form-control" type="text" name="phone" value="<?php echo (empty($posted['phone'])) ? '' : $posted['phone']; ?>" /></td>
         </tr>
         <tr>
           <td>Product Info <span class="mand">*</span>: </td>
-          <td colspan="3"><textarea name="productinfo" style="width: 100%;"><?php echo (empty($posted['productinfo'])) ? '' : $posted['productinfo'] ?></textarea></td>
+          <td colspan="3"><textarea class="form-control" name="productinfo" style="width: 100%;"><?php echo (empty($posted['productinfo'])) ? '' : $posted['productinfo'] ?></textarea></td>
         </tr>
         <tr class="hd">
           <td>Success URL <span class="mand">*</span>: </td>
-          <td colspan="3"><input type="text" name="surl" value="<?php echo (empty($posted['surl'])) ? $currentDir.'PayU_success.php' : $posted['surl'] ?>" size="64" /></td>
+          <td colspan="3"><input class="form-control" type="text" name="surl" value="<?php echo (empty($posted['surl'])) ? $currentDir.'PayU_success.php' : $posted['surl'] ?>" size="64" /></td>
         </tr>
         <tr class="hd">
           <td>Failure URL <span class="mand">*</span>: </td>
-          <td colspan="3"><input type="text" name="furl" value="<?php echo (empty($posted['furl'])) ? $currentDir.'PayU_failure.php' : $posted['furl'] ?>" size="64" /></td>
+          <td colspan="3"><input class="form-control" type="text" name="furl" value="<?php echo (empty($posted['furl'])) ? $currentDir.'PayU_failure.php' : $posted['furl'] ?>" size="64" /></td>
         </tr>
 
         <tr>
@@ -117,45 +138,45 @@
         </tr>
         <tr class="hd">
           <td>Last Name: </td>
-          <td><input type="text" name="lastname" id="lastname" value="<?php echo (empty($posted['lastname'])) ? '' : $posted['lastname']; ?>" /></td>
+          <td><input class="form-control" type="text" name="lastname" id="lastname" value="<?php echo (empty($posted['lastname'])) ? '' : $posted['lastname']; ?>" /></td>
           <td>Cancel URI: </td>
-          <td><input type="text" name="curl" value="" /></td>
+          <td><input class="form-control" type="text" name="curl" value="" /></td>
         </tr>
         <tr class="hd">
           <td>Address1: </td>
-          <td><input type="text" name="address1" value="<?php echo (empty($posted['address1'])) ? '' : $posted['address1']; ?>" /></td>
+          <td><input  class="form-control" type="text" name="address1" value="<?php echo (empty($posted['address1'])) ? '' : $posted['address1']; ?>" /></td>
           <td>Address2: </td>
-          <td><input type="text" name="address2" value="<?php echo (empty($posted['address2'])) ? '' : $posted['address2']; ?>" /></td>
+          <td><input class="form-control" type="text" name="address2" value="<?php echo (empty($posted['address2'])) ? '' : $posted['address2']; ?>" /></td>
         </tr>
         <tr class="hd">
           <td>City: </td>
-          <td><input type="text" name="city" value="<?php echo (empty($posted['city'])) ? '' : $posted['city']; ?>" /></td>
+          <td><input class="form-control" type="text" name="city" value="<?php echo (empty($posted['city'])) ? '' : $posted['city']; ?>" /></td>
           <td>State: </td>
-          <td><input type="text" name="state" value="<?php echo (empty($posted['state'])) ? '' : $posted['state']; ?>" /></td>
+          <td><input class="form-control" type="text" name="state" value="<?php echo (empty($posted['state'])) ? '' : $posted['state']; ?>" /></td>
         </tr>
         <tr class="hd">
           <td>Country: </td>
-          <td><input type="text" name="country" value="<?php echo (empty($posted['country'])) ? '' : $posted['country']; ?>" /></td>
+          <td><input class="form-control" type="text" name="country" value="<?php echo (empty($posted['country'])) ? '' : $posted['country']; ?>" /></td>
           <td>Zipcode: </td>
-          <td><input type="text" name="zipcode" value="<?php echo (empty($posted['zipcode'])) ? '' : $posted['zipcode']; ?>" /></td>
+          <td><input class="form-control" type="text" name="zipcode" value="<?php echo (empty($posted['zipcode'])) ? '' : $posted['zipcode']; ?>" /></td>
         </tr>
         <tr class="hd">
           <td>UDF1: </td>
-          <td><input type="text" name="udf1" value="<?php echo (empty($posted['udf1'])) ? '' : $posted['udf1']; ?>" /></td>
+          <td><input class="form-control" type="text" name="udf1" value="<?php echo (empty($posted['udf1'])) ? '' : $posted['udf1']; ?>" /></td>
           <td>UDF2: </td>
-          <td><input type="text" name="udf2" value="<?php echo (empty($posted['udf2'])) ? '' : $posted['udf2']; ?>" /></td>
+          <td><input class="form-control" type="text" name="udf2" value="<?php echo (empty($posted['udf2'])) ? '' : $posted['udf2']; ?>" /></td>
         </tr>
         <tr class="hd">
           <td>UDF3: </td>
-          <td><input type="text" name="udf3" value="<?php echo (empty($posted['udf3'])) ? '' : $posted['udf3']; ?>" /></td>
+          <td><input class="form-control" type="text" name="udf3" value="<?php echo (empty($posted['udf3'])) ? '' : $posted['udf3']; ?>" /></td>
           <td>UDF4: </td>
-          <td><input type="text" name="udf4" value="<?php echo (empty($posted['udf4'])) ? '' : $posted['udf4']; ?>" /></td>
+          <td><input class="form-control" type="text" name="udf4" value="<?php echo (empty($posted['udf4'])) ? '' : $posted['udf4']; ?>" /></td>
         </tr>
         <tr class="hd">
           <td>UDF5: </td>
-          <td><input type="text" name="udf5" value="<?php echo (empty($posted['udf5'])) ? '' : $posted['udf5']; ?>" /></td>
+          <td><input class="form-control" type="text" name="udf5" value="<?php echo (empty($posted['udf5'])) ? '' : $posted['udf5']; ?>" /></td>
           <td>PG: </td>
-          <td><input type="text" name="pg" value="<?php echo (empty($posted['pg'])) ? '' : $posted['pg']; ?>" /></td>
+          <td><input class="form-control" type="text" name="pg" value="<?php echo (empty($posted['pg'])) ? '' : $posted['pg']; ?>" /></td>
         </tr>
         <tr>
           <?php if(!$hash) { ?>
